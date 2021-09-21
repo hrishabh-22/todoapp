cc4f836418284b64b71732ea006ec63ccc1d4969
hrishabh <hrishabhm22@gmail.com>
2021-09-21 21:39:18 +0530

    App.js Updated

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StatusBar} from 'react-native';
+
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {Provider} from 'react-redux';
+import store from './store';
+import Home from './screens/Home';
+import Add from './screens/Add';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <Text>Hello From App.js</Text>;
+  return (
+    <Provider store={store}>
+      <StatusBar backgroundColor="#0f4c75" />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Add"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#0f4c75',
+            },
+            title: 'Todo App',
+            headerTitleStyle: {
+              textAlign: 'center',
+              color: '#00b7c2',
+            },
+          }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Add" component={Add} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
 };
 
 export default App;
