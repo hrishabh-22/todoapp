271e5026c707f17e7ea34f5639f7ac644e7f4a44
hrishabh <hrishabhm22@gmail.com>
2021-09-21 14:52:40 +0530

    Frontend Started

---
+++ todoapp/src/screens/Add.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Text} from 'native-base';
+
+const Add = props => {
+  return <Text>Hello From Add.js</Text>;
+};
+
+export default Add;
---
+++ todoapp/src/screens/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Text} from 'native-base';
+
+const Home = props => {
+  return <Text>Hello From Home.js</Text>;
+};
+
+export default Home;
