c6fff9e2548164e2fef53d9c03775894f87bd3a3
hrishabh <hrishabhm22@gmail.com>
2021-09-22 19:24:11 +0530

    V 1.0.0 Completed

--- todoapp/src/screens/Home.js
+++ todoapp/src/screens/Home.js
@@ -16,13 +16,15 @@ import {
   Container,
   Content,
 } from 'native-base';
+import propTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {removeTodo, markComplete} from '../action/list';
 
 const Home = props => {
-  let listState = [];
   return (
     <Container>
       <ScrollView contentContainerStyle={styles.container}>
-        {listState.length === 0 ? (
+        {props.listState.length === 0 ? (
           <Container style={styles.emptyContainer}>
             <H1 style={styles.heading}>Add Something</H1>
           </Container>
@@ -30,10 +32,15 @@ const Home = props => {
           <Content padder>
             <H1 style={styles.heading}>Next Todo</H1>
             <List>
-              {listState.map(todo => (
+              {props.listState.map(todo => (
                 <ListItem icon key={todo.id} noBorder style={styles.listItem}>
                   <Left>
-                    <Button danger style={styles.actionButton}>
+                    <Button
+                      danger
+                      style={styles.actionButton}
+                      onPress={() => {
+                        props.removeTodo(todo.id);
+                      }}>
                       <Icon active name="trash" />
                     </Button>
                   </Left>
@@ -42,7 +49,12 @@ const Home = props => {
                     <Text note>{todo.description}</Text>
                   </Body>
                   <Right>
-                    <CheckBox checked={todo.isDone} />
+                    <CheckBox
+                      checked={todo.isDone}
+                      onPress={() => {
+                        props.markComplete(todo.id);
+                      }}
+                    />
                   </Right>
                 </ListItem>
               ))}
@@ -52,8 +64,7 @@ const Home = props => {
         <Fab
           style={{backgroundColor: '#5067FF'}}
           position="bottomRight"
-          //onPress={() => navigation.navigate('Add')}
-        >
+          onPress={() => props.navigation.navigate('Add')}>
           <Icon name="add" />
         </Fab>
       </ScrollView>
@@ -61,7 +72,22 @@ const Home = props => {
   );
 };
 
-export default Home;
+const mapStateToProps = state => ({
+  listState: state.list,
+});
+
+const mapDispatchToProps = {
+  removeTodo: id => removeTodo(id),
+  markComplete: id => markComplete(id),
+};
+
+Home.propTypes = {
+  removeTodo: propTypes.func.isRequired,
+  markComplete: propTypes.func.isRequired,
+  listState: propTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
 
 const styles = StyleSheet.create({
   container: {
